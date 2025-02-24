5c3119499b590e4b660b91d9a4dab33f185a1128
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2025-02-24 19:31:05 +0000

    [$] Bumped version to 0.1.1 and updated setup.py to include package data.

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="DaggyD",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["DaggyD.daggyd"],
     python_requires=">=3.12",
+    include_package_data=True,  # Force additional files into the package
 )
