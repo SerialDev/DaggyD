12b3720921264018e106210444a0f3bbb075f3a4
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2025-02-24 18:30:35 +0000

    [-] removed example_function import and test from test_DaggyD.py

--- DaggyD/tests/test_DaggyD.py
+++ DaggyD/tests/test_DaggyD.py
@@ -1,9 +1,4 @@
 import unittest
-from DaggyD import example_function
-
-class TestDaggyD(unittest.TestCase):
-    def test_example_function(self):
-        self.assertEqual(example_function(), "Hello from DaggyD!")
 
 if __name__ == "__main__":
     unittest.main()
